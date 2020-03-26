293e53d8ef7fe5a4f6339006722763c14babea0d
clhZhou <249117598@qq.com>
2020-03-26 21:33:25 +0800

    init

---
+++ src/test/java/com/luis/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luis.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
